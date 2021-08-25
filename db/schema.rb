55f332521b9b73892fe48135adde5675dde2af71
“Tony <adrprogramming@gmail.com>
2021-08-24 20:57:42 -0600

    first heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_27_194158) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "beers", force: :cascade do |t|
     t.string "name"
     t.string "style"
