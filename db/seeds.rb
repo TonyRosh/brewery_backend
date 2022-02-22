826eb5141bff0b1cf175a692be2850224305a593
“Tony <adrprogramming@gmail.com>
2022-02-21 17:30:50 -0700

    fixed foods

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
 Beer.destroy_all
 Food.destroy_all
 
-8.times do
-    Food.create(dish: Faker::Food.dish, description: Faker::Food.description)
-end
+Food.create(dish: 'The Trio', description: 'Snacks')
+Food.create(dish: 'Pretzel Bites and Beer Cheese Queso', description: 'Snacks')
+Food.create(dish: 'Cheese Board', description: 'Snacks')
+Food.create(dish: 'Flatbread and Hummus', description: 'Snacks')
+Food.create(dish: 'White Cheddar Mac n Cheese', description: 'Snacks')
+Food.create(dish: 'Nachos', description: 'Snacks')
+Food.create(dish: 'Soupe of the day', description: 'Snacks')
+Food.create(dish: 'Cheese', description: 'Pizza')
+Food.create(dish: 'Pepperoni', description: 'Pizza')
+Food.create(dish: 'Sausage and Pepperoni', description: 'Pizza')
+Food.create(dish: 'Deluxe', description: 'Pizza')
+Food.create(dish: 'BBQ Chicken', description: 'Dillas')
+Food.create(dish: 'Buffalo Chicken', description: 'Dillas')
+Food.create(dish: 'Carnitas', description: 'Dillas')
+Food.create(dish: 'Chicekn Fajitas', description: 'Dillas')
+Food.create(dish: 'BBQ Pulled Pork', description: 'Sandwiches')
+Food.create(dish: 'Meatball Grinder', description: 'Sandwiches')
+Food.create(dish: 'Black Bean w/ Tzatziki', description: 'Sandwiches')
 
 Beer.create(name: 'Bobcat Lager', style: 'Lager', alcohol: '4.9%', ibu: 17, description: Faker::Lorem.paragraphs)
 Beer.create(name: 'Black Bear', style: 'Milk Stout', alcohol: '7%', ibu: 19, description: Faker::Lorem.paragraphs)
